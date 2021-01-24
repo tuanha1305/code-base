75a80eb25585fc8efefcc20385cd6981766a4277
tan.dao <tan.dao@vexere.com>
2021-01-24 19:03:18 +0700

    Refactor code

--- app/configs/colors.js
+++ app/configs/colors.js
@@ -1,8 +1,9 @@
 export default {
     statusBarColor: '#FFFFFF',
 
-    primaryColor: '#008B00',
-    secondaryColor: '#E8F5E9',
+    primaryColor: '#6200EE',
+    secondaryColor: '#3700B3',
+    tertiaryColor: '#BB86FC',
 
     backgroundColor: '#DEDEDE',
     contentColor: '#F2F2F2',
@@ -10,9 +11,12 @@ export default {
     hideColor: '#DBDBDB',
     lineColor: '#DADADA',
 
-    primaryTextColor: '#000000',
+    primaryTextColor: '#1D1D1D',
     secondaryTextColor: '#979797',
     placeholderTextColor: '#9B9B9B',
+    titleTextColor: '#707070',
+    noteTextColor: '#979797',
+    errorTextColor: '#DD2C00',
 
     white: '#FFFFFF',
     black: '#000000'
--- app/configs/styles.js
+++ app/configs/styles.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import colors from 'configs/colors';
 import dimens from 'configs/dimens';
 
