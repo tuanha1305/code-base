a17e8f301c9468ae3fdcef167be4c896a6524155
tan.dao <tan.dao@vexere.com>
2021-04-04 22:25:05 +0700

    Add lib

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import 'react-native-get-random-values';
 import {AppRegistry} from 'react-native';
 import CodePush from 'react-native-code-push';
 import {name as appName} from './app.json';
